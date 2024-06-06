4dcfa6aced476b2db1574141a97a89e9c8c7fb1d
coderyu5523 <coderyu5523@naver.com>
2024-06-06 15:40:09 +0900

    [fix] dummy data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,14 +4,14 @@ insert into user_tb(username,password,email,phone,created_at) values ('user2','1
 insert into user_tb(username,password,email,phone,created_at) values ('user3','1234','user3@nate.com','01033334444',now());
 insert into user_tb(username,password,email,phone,created_at) values ('user4','1234','user4@nate.com','01044445555',now());
 
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목1','내용1','스포츠','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',1,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목2','내용2','스포츠','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',2,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목3','내용3','게임','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',3,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목4','내용4','게임','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',4,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목5','내용5','게임','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',5,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목6','내용6','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',1,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목7','내용7','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',2,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목8','내용8','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',3,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목9','내용9','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',4,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목10','내용10','영화','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',5,now());
-insert into board_tb(title,content,category,board_img,user_id,created_at) values ('제목11','내용11','영화','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',1,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('스포츠 제목1','스포츠 내용1','스포츠','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',1,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('스포츠 제목2','스포츠 내용2','스포츠','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',2,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('게임 제목3','게임 내용3','게임','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',3,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('게임 제목4','게임 내용4','게임','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',4,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('게임 제목5','게임 내용5','게임','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',5,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('음식 제목6','음식 내용6','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',1,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('음식 제목7','음식 내용7','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',2,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('음식 제목8','음식 내용8','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',3,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('음식 제목9','음식 내용9','음식','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',4,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('영화 제목10','영화 내용10','영화','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',5,now());
+insert into board_tb(title,content,category,board_img,user_id,created_at) values ('영화 제목11','영화 내용11','영화','/upload/0bcd001f-81d2-4c08-b79c-2d249c27ae3c.jpg',1,now());
